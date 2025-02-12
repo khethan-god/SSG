fcecf8c75dc8f67d907b2dfa41d6e1c45483b70f
khethan-god <khethanrg03@gmail.com>
2025-02-12 10:07:09 +0530

    2: wrote a function to convert text to HTML tags

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,43 @@ class HTMLNode:
     
     def __repr__(self):
         return f'HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, property={self.props})'
+    
+
+# First child class which represents the leaf nodes
+# It will not have any children
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("invalid HTML: no value")
+        if not self.tag:
+            return self.value
+        if not self.props:
+            return f'<{self.tag}>{self.value}</{self.tag}>'
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    
+    def __repr__(self):
+        return f"LeafNode(tag={self.tag}, value={self.value}, property={self.props})"
+
+
+# Second child class which represents the parent nodes
+# It must have children
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def __repr__(self):
+        return f"ParentNode(tag={self.tag}, children={self.children}, property={self.props})"
+    
+    def to_html(self):
+        if not self.tag: raise ValueError("invalid HTML: no tag found")
+        if not self.children: raise ValueError("invalid HTML: no inline tags found")
+        children_html = ""
+        # Here if the child nodes have parent nodes within them then
+        # recursion happens, if not then the `.to_html()` function of the
+        # leaf node is called
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from htmlnode import HTMLNode
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 
 def main():
