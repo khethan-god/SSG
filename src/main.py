70969a9be3969a373e0a13d2327c12b67e9cd2ba
khethan-god <khethanrg03@gmail.com>
2025-03-25 16:26:55 +0530

    C: changed the root location so to host in github pages

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import shutil
 
 from copystatic import copy_files_recursive
@@ -6,12 +7,14 @@ from gencontent import generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
+    basepath = sys.argv[1] if len(sys.argv)>0 else "/"
+    print(basepath)
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -20,7 +23,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
